13cc5ddb5eaa910d84ebb8c1da525f07f5f19dad
Mingbao Lin 林明宝 <linmb001@outlook.com>
2025-04-17 15:49:31 +0800

    add training verification

--- training/train.py
+++ training/train.py
@@ -34,3 +34,17 @@ def train_model(model, dataloader, num_epochs, lr, device = "cuda", use_amp = Fa
             scaler.update()
             total_loss += loss.item()
         print(f"Epoch {epoch + 1} Loss: {total_loss / len(dataloader):.4f}")
+
+
+
+if __name__ == "__main__":
+    from ..models.simple_rnn import LM
+    from ..data.dataset import CharDataset, collate_batch
+    from transformers import AutoTokenizer
+    from torch.utils.data import DataLoader
+
+    tokenizer = AutoTokenizer.from_pretrained("gpt2") 
+    dataset = CharDataset("Hello world! This is a test to verify the correctness of dataloader.", tokenizer, seq_len = 8)
+    dataloader = DataLoader(dataset, batch_size = 4, shuffle = True, collate_fn = collate_batch)
+    model = LM(tokenizer.vocab_size, hidden_dim= 128, key_dim = 32, value_dim = 32, output_dim = 64, num_layers = 2)
+    train_model(model, dataloader, num_epochs = 1, lr = 1e-3, device = "cpu")
