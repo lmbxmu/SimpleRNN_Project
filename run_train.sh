7cd8a8943919b1830eefa022b65f64c880fe9335
Mingbao Lin 林明宝 <linmb001@outlook.com>
2025-04-17 16:15:17 +0800

    add run_train.sh

---
+++ run_train.sh
@@ -0,0 +1,9 @@
+# Train the model
+
+python3 run_train.py \
+   --text "Hello world! This is a test to verify the correctness of dataloader." \
+   --seq_len 8 \
+   --batch_size 4 \
+   --num_epochs 5 \
+   --lr 0.001 \
+   --device gpu
