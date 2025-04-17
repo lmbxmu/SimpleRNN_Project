2c40ed8061ff80aa008377b93a54691c98b42a5e
Mingbao Lin 林明宝 <linmb001@outlook.com>
2025-04-17 16:04:30 +0800

    add decode verification

--- inference/decode.py
+++ inference/decode.py
@@ -17,3 +17,11 @@ def greedy_decode(model, tokenizer, prompt: str, max_length: int = 50, device = 
             next_token = logits[:, -1, :].argmax(dim = -1, keepdim = True)
             input_ids = torch.cat([input_ids, next_token], dim = 1)
     return tokenizer.decode(input_ids[0], skip_special_tokens = True)
+
+
+if __name__ == "__main__":
+    from ..models.simple_rnn import LM
+    from transformers import AutoTokenizer
+    tokenizer = AutoTokenizer.from_pretrained("gpt2")
+    model = LM(tokenizer.vocab_size, hidden_dim= 128, key_dim = 32, value_dim = 32, output_dim = 64, num_layers = 2)
+    print(greedy_decode(model, tokenizer, "hello", max_length = 0, device = "cpu"))
