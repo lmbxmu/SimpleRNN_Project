463fbd507c0a5d5f0b5fbfba3f281daf455a89a2
Mingbao Lin 林明宝 <linmb001@outlook.com>
2025-04-17 16:37:36 +0800

    update run_trian.py via argument

--- run_train.py
+++ run_train.py
@@ -12,9 +12,26 @@ from data.dataset import CharDataset, collate_batch
 from transformers import AutoTokenizer
 from torch.utils.data import DataLoader
 
-if __name__ == "__main__":
+import argparse
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Train SimpleRNN-based LM")
+    parser.add_argument("--text", type=str, required=True, help="Training text content")
+    parser.add_argument("--seq_len", type=int, default=8, help="Sequence length")
+    parser.add_argument("--batch_size", type=int, default=4, help="Batch size")
+    parser.add_argument("--num_epochs", type=int, default=5, help="Number of epochs")
+    parser.add_argument("--lr", type=float, default=1e-3, help="Learning rate")
+    parser.add_argument("--device", type=str, default="cuda", help="Device to train on")
+    args = parser.parse_args()
+
     tokenizer = AutoTokenizer.from_pretrained("gpt2") 
-    dataset = CharDataset("Hello world! This is a test to verify the correctness of dataloader.", tokenizer, seq_len = 8)
-    dataloader = DataLoader(dataset, batch_size = 4, shuffle = True, collate_fn = collate_batch)
-    model = LM(tokenizer.vocab_size, hidden_dim= 128, key_dim = 32, value_dim = 32, output_dim = 64, num_layers = 2)
-    train_model(model, dataloader, num_epochs = 5, lr = 1e-3)
+    dataset = CharDataset(args.text, tokenizer, seq_len = args.seq_len)
+    dataloader = DataLoader(dataset, batch_size = args.batch_size, shuffle = True, collate_fn = collate_batch)
+
+    model = LM(vocab_size = tokenizer.vocab_size, hidden_dim = 128, key_dim = 32, value_dim = 32, output_dim = 64, num_layers = 2)
+    train_model(model, dataloader, num_epochs = args.num_epochs, lr = args.lr, device = args.device)
+
+
+if __name__ == "__main__":
+    main()
