2971b417d911211f2ee94c22eb7c52c1c57591bb
lmbxmu <linmb001@outlook.com>
2025-04-21 00:37:29 +0800

    update run_train.py

--- run_train.py
+++ run_train.py
@@ -13,23 +13,28 @@ from transformers import AutoTokenizer
 from torch.utils.data import DataLoader
 
 import argparse
+import json
 
 
 def main():
     parser = argparse.ArgumentParser(description="Train SimpleRNN-based LM")
-    parser.add_argument("--text", type=str, required=True, help="Training text content")
-    parser.add_argument("--seq_len", type=int, default=8, help="Sequence length")
+    parser.add_argument("--file_path", type=str, required=True, help="Training text content")
     parser.add_argument("--batch_size", type=int, default=4, help="Batch size")
     parser.add_argument("--num_epochs", type=int, default=5, help="Number of epochs")
     parser.add_argument("--lr", type=float, default=1e-3, help="Learning rate")
     parser.add_argument("--device", type=str, default="cuda", help="Device to train on")
     args = parser.parse_args()
 
-    tokenizer = AutoTokenizer.from_pretrained("gpt2") 
-    dataset = CharDataset(args.text, tokenizer, seq_len = args.seq_len)
+    text = []
+    with open(args.file_path, "r") as f:
+        for line in f:
+            data = json.loads(line)
+            text.append(data["bot"])
+    tokenizer = AutoTokenizer.from_pretrained("Qwen/Qwen-tokenizer")
+    dataset = CharDataset(text, tokenizer)
     dataloader = DataLoader(dataset, batch_size = args.batch_size, shuffle = True, collate_fn = collate_batch)
 
-    model = LM(vocab_size = tokenizer.vocab_size, hidden_dim = 128, key_dim = 128, value_dim = 128, output_dim = 128, num_layers = 2)
+    model = LM(vocab_size = tokenizer.vocab_size + 1, hidden_dim = 128, key_dim = 128, value_dim = 128, output_dim = 128, num_layers = 2)
     train_model(model, dataloader, num_epochs = args.num_epochs, lr = args.lr, device = args.device)
 
 
