01edf57be2f13653d6831e2fbc730b6c7020080b
lmbxmu <linmb001@outlook.com>
2025-04-21 10:17:29 +0800

    update run_train.sh

--- run_train.sh
+++ run_train.sh
@@ -3,7 +3,8 @@
 
 python3 run_train.py \
    --file_path ./test_dataset/training.jsonl \
+   --save_every_n_epochs 5 \
    --batch_size 4 \
-   --num_epochs 10 \
+   --num_epochs 20 \
    --lr 0.001 \
    --device cuda
